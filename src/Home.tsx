2f3536852129bef1bd80274595a10458fadb3ccb
Virgil <105520391+VirgiliuVV@users.noreply.github.com>
2023-05-16 09:00:49 +0300

    Laboratorul 7

---
+++ src/Home.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+    return(
+        <div className="start_img">
+            <div className="container">
+                <div className="homeImage"></div>
+            </div>
+        </div>
+    );
+}
---
+++ src/SocialMedia.tsx
@@ -0,0 +1,15 @@
+export function SocialMedia() {
+    return(
+        <div className="containerS">
+            <nav className="menu">
+                <div id="title">Social Media</div>
+                <br/>
+                <a href="https://facebook.com">Facebook</a>
+                <a href="https://www.instagram.com">Instagram</a>
+                <a href="https://twitter.com">Twitter</a>
+                <a href="https://telegram.org">Telegram</a>
+            </nav>
+        </div>
+
+    );
+}
